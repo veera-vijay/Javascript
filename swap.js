750b8de7037eab3de469b4376ec1b6737d62c725
veera-vijay <247803116+veera-vijay@users.noreply.github.com>
2026-02-17 18:59:52 +0530

    Add files via upload

---
+++ swap.js
@@ -0,0 +1,14 @@
+let a=5
+let b=10
+console.log("Before swap a= "+a+ "and b= "+b)
+
+// let temp=a
+// a=b
+// b=temp
+// console.log("After swap a="+a+"and b="+b)
+
+a=a+b
+b=a-b
+a=a-b
+console.log(a)
+console.log(b)
