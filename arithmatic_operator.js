abab5c91e5b60d288ebed509d4c71d1795e3daa8
veera-vijay <247803116+veera-vijay@users.noreply.github.com>
2026-02-17 18:41:59 +0530

    Add files via upload

---
+++ arithmatic_operator.js
@@ -0,0 +1,29 @@
+//add
+let a=10
+let b=20
+console.log(a+b)
+
+//sub
+let c = 10;
+let d = 20;
+console.log(c - d);
+
+//multiply
+let e = 10;
+let f = 20;
+console.log(e * f);
+
+//division
+let g = 20;
+let h = 5;
+console.log(g / h);
+
+//Modulus
+let i= 20;
+let j = 50;
+console.log(i % j);
+
+//Exponent
+let k=3;
+let l=2
+console.log(k**l)
