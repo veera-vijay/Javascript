ee16ebdcf8ed5a23069729c49f7a325cc61aa496
veera-vijay <247803116+veera-vijay@users.noreply.github.com>
2026-02-17 18:54:38 +0530

    Add files via upload

---
+++ module_import.js
@@ -0,0 +1,9 @@
+//  import {add} from './first.js';
+//  let result = add(2,3)
+//  console.log(result)
+
+import {add ,sub ,multiply} from './module_export.js';
+console.log(add(28,38))
+console.log(sub(234,342))
+console.log(multiply(5,2))
+
