18a064de77ecc08d435708cec4ee3fea2c0e9780
veera-vijay <247803116+veera-vijay@users.noreply.github.com>
2026-02-17 18:42:25 +0530

    Add files via upload

---
+++ asignment_operators.js
@@ -0,0 +1,31 @@
+
+let a=10
+a+=5
+console.log(a)
+
+//-=
+
+let b=20
+b-=5
+console.log(b)
+
+//*=
+let c=5
+c*=3
+console.log(c)
+
+///=
+
+let d=30
+d/=2
+console.log(d)
+
+//%=
+let f=20
+f%=5
+console.log(f)
+
+//**= 
+let g=5
+g**=3
+console.log(g)
