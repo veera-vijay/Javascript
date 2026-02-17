fdb36e2ba5a865ada78abe19cada7ee0df9da9c8
veera-vijay <247803116+veera-vijay@users.noreply.github.com>
2026-02-17 18:41:13 +0530

    Add files via upload

---
+++ const.js
@@ -0,0 +1,30 @@
+//inizialization
+
+const a = 10;
+console.log(a);
+
+//declare is not possible
+// const c                             //SyntaxError: Missing initializer in const declaration
+ //c=10
+ //console.log(c)
+
+//reassign    is not possible
+// const b=10;                          //SyntaxError: Missing initializer in const declaration
+// const b=20;
+// console.log(b)
+
+//hosting is not possible
+// y=10
+// console.log(y);                         //SyntaxError: Missing initializer in const declaration
+// const y
+
+function test() {
+    const a = 5;
+    const z = 10;
+    console.log(a);
+    console.log(z);
+ 
+}
+test();
+
+
