35959767865876ef7dccd25534cf551d93e3d09d
veera-vijay <247803116+veera-vijay@users.noreply.github.com>
2026-02-17 18:39:40 +0530

    Add files via upload

---
+++ var.js
@@ -0,0 +1,34 @@
+//intialization
+
+var num=10
+console.log(num)
+
+//declaraization
+
+var a;
+a=30;
+console.log(a);
+
+// reassign
+
+var a;
+a=30;
+a=40;
+console.log(a)
+
+//hosting
+
+b=10;
+console.log(b);
+var b;
+
+
+//function block
+var a='veera'
+if (true) {
+  var a = 'vijay';
+}
+console.log(a);
+
+
+
