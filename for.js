913f15fa2d277f33171ba11c7270943a3aa1a1c9
veera-vijay <247803116+veera-vijay@users.noreply.github.com>
2026-02-17 18:44:01 +0530

    Add files via upload

---
+++ for.js
@@ -0,0 +1,10 @@
+// 
+// let a=629
+// let b=10
+// console.log(a%b)
+const prompt = require("prompt-sync")();
+
+let a = Number(prompt("a =  "));
+let b = Number(prompt("b =  "));
+
+console.log("Sum =", a + b);
