f657212dd5cfeb941bcb67e8a40ba117fc285910
veera-vijay <247803116+veera-vijay@users.noreply.github.com>
2026-02-17 18:44:53 +0530

    Add files via upload

---
+++ if.js
@@ -0,0 +1,9 @@
+let value=16
+if( value>=16 && value<=20)
+  console.log("overload")
+else if(value>=21 && value <=25)
+  console.log("normal")
+else if(value>=26  && value<=30)
+  console.log("cold")
+else
+  console.log("value out of range")
