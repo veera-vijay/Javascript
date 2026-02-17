f835af58a286a8d69843210fca4e9e7d40d03aa3
veera-vijay <247803116+veera-vijay@users.noreply.github.com>
2026-02-17 18:55:30 +0530

    Add files via upload

---
+++ namespace_import.js
@@ -0,0 +1,5 @@
+import * as vijay from './namespace_export.js';
+
+console.log("the num is:"+vijay.num)
+console.log("add a and b is :"+vijay.add(2,2))
+console.log("multiply a and b is:"+vijay.multiply(5,3))
