0aafc34805d0650ce8e3324f416957339231e3c4
veera-vijay <247803116+veera-vijay@users.noreply.github.com>
2026-02-17 18:58:14 +0530

    Add files via upload

---
+++ spread.js
@@ -0,0 +1,10 @@
+let arr=['html','css','javascrpit','react']
+let arr2=['nodejs','express js','mangodb']
+let res=[...arr,...arr2]
+console.log(res)
+
+
+let numbers=[1,2,3,4,5]
+let numbers1=[6,7,8,9,10]
+let res1=[...numbers,...numbers1]
+console.log(res1)
