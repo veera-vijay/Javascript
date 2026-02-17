869de846cf72e680b5912040b11af4e9b1ca33de
veera-vijay <247803116+veera-vijay@users.noreply.github.com>
2026-02-17 18:43:34 +0530

    Add files via upload

---
+++ comparison_operators.js
@@ -0,0 +1,36 @@
+//==
+let a = 5;
+let b = 5;
+if (a == b) console.log("true");
+
+//====
+let c = 4;
+let d = 4;
+if (c === d) console.log("true");
+
+//!=
+
+let e = 7;
+let f = 9;
+if (e != f) console.log("true");
+//>
+let g = 13;
+let h = 10;
+if (g > h) console.log("true");
+
+//<
+let i = 1;
+let j = 10;
+if (i < j) console.log("true");
+//>=
+let k = 13;
+let l = 10;
+if (k >= l) 
+     console.log(" k>=than ltrue");
+
+//<=
+let m = 13;
+let n = 16;
+if (m <= n) 
+    console.log("true");
+
