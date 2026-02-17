9735174fb62a694bdc0c8207658e8f7abcf3c468
veera-vijay <247803116+veera-vijay@users.noreply.github.com>
2026-02-17 18:46:18 +0530

    Add files via upload

---
+++ expresion_function.js
@@ -0,0 +1,13 @@
+
+
+//before hoisting is not possile
+// greet();
+//  const greet = function sayHello(name) {
+//   console.log( 'hello');
+//  };
+
+const value=function name(a,b){
+     console.log(a+b);
+}
+value(5,5)
+
